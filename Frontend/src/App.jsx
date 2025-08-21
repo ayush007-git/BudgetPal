0cac8644c00576ab21edfd06760dceabfe7a96ae
Ayush Kumar Himanshu <akh784596@gmail.com>
2025-08-21 18:04:28 +0530

    Revert " feat(auth): add Login, Signup, and Landing pages"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import LandingPage from './pages/Landingpage'; 
-import Login from './pages/Login';
-import Signup from './pages/Signup'; 
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-        
-          <Route path="/" element={<LandingPage />} />
-          
-          <Route path="/login" element={<Login />} />
-          
-          <Route path="/signup" element={<Signup />} />
-          
-          <Route path="/home" element={<Navigate to="/" replace />} />
-          
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    <div>
+      Frontend is runing
+    </div>
+  )
 }
-
-export default App;
